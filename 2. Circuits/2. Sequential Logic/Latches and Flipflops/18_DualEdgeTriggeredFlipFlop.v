2042bfb628efe18a45160fc02290cfa97571fbbf
Santhosh <santhoshofficial0001@gmail.com>
2026-09-09 20:10:55 +0530

    Create 18_DualEdgeTriggeredFlipFlop.v

--- 2. Circuits/2. Sequential Logic/Latches and Flipflops/18_DualEdgeTriggeredFlipFlop.v
+++ 2. Circuits/2. Sequential Logic/Latches and Flipflops/18_DualEdgeTriggeredFlipFlop.v
@@ -1,14 +1,14 @@
 module top_module (
     input clk,
     input d,
-    output reg q
+    output q
 );
     reg [1:0]temp;
     always@(posedge clk) begin
-        temp[0] = d;
+        temp[0] <= d;
     end
     always@(negedge clk) begin
-        temp[1] = d;
+        temp[1] <= d;
     end
     assign q = clk ? temp[0] : temp[1];
 endmodule
