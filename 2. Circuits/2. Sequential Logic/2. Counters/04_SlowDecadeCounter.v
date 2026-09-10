dc02b67a5b4599f7fd494cab71cfa2339db6561d
Santhosh <santhoshofficial0001@gmail.com>
2026-09-10 20:31:52 +0530

    Create 04_SlowDecadeCounter.v

---
+++ 2. Circuits/2. Sequential Logic/2. Counters/04_SlowDecadeCounter.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    
+    always@(posedge clk)  begin
+        if(reset | (q == 4'b1001 & slowena)) 
+            q <= 4'b0;
+        else if(slowena)
+            q <= q + 1'b1;
+    end
+    
+endmodule
