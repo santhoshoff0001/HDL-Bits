bbfc0b8c17904e65f792c22c342c4a9cc63661ef
Santhosh <santhoshofficial0001@gmail.com>
2026-09-11 23:22:05 +0530

    Create 05_Counter1-12.v

---
+++ 2. Circuits/2. Sequential Logic/2. Counters/05_Counter1-12.v
@@ -0,0 +1,25 @@
+module top_module (
+    input clk,
+    input reset,
+    input enable,
+    output [3:0] Q,
+    output c_enable,
+    output c_load,
+    output [3:0] c_d
+); 
+    assign c_d = 4'b0001;
+    assign c_enable = enable;
+	assign c_load = reset | (enable & (Q == 4'd12));
+
+   
+    count4 the_counter (
+        .clk(clk),
+        .enable(c_enable),
+        .load(c_load),
+        .d(c_d),
+        .Q(Q)
+    );
+
+   
+
+endmodule
